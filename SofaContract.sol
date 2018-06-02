2bdc00c1ba5a327d76801104dd4fd219f1153fc2
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-06-02 08:31:48 -0700

    add basic practice contracts... ;)

---
+++ SofaContract.sol
@@ -0,0 +1,28 @@
+pragma solidity^0.4.0;
+
+contract SofaContract {
+
+  struct Sofa {
+    string name;
+    uint seats;
+  }
+
+  Sofa[] sofas;
+  mapping(uint => address) sofaToOwner;
+
+  modifier isOwner(uint _id) {
+    require(msg.sender == sofaToOwner[_id]);
+    _;
+  }
+
+  function addSofa(string _name, uint _seats) public returns (uint) {
+    address owner = msg.sender;
+    uint id = sofas.push(Sofa(_name, _seats));
+    sofaToOwner[id] = owner;
+    return id;
+  }
+
+  function getSofa(uint _id) isOwner(_id) view public returns (string) {
+    return sofas[_id - 1].name;
+  }
+}
