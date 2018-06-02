d3a3ab1e2ee82511b94cd025ef8777a4380574e6
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2018-06-02 12:33:07 -0700

    add basic inheritance patterns and SafeMath for calculations

---
+++ SofaStoreContract.sol
@@ -0,0 +1,29 @@
+pragma solidity^0.4.18;
+
+import './SofaContract.sol';
+import './SafeMath.sol';
+
+contract SofaStoreContract is SofaContract {
+  using SafeMath for uint;
+
+  modifier assureOwnerShip(uint _sofaId) {
+    require(msg.sender == sofaToOwner[_sofaId]);
+    _;
+  }
+
+  function transferSofa(uint _sofaId, address _newOwner)
+    assureOwnerShip(_sofaId)
+    public
+  {
+    sofaToOwner[_sofaId] = _newOwner;
+  }
+
+  function findAverageSeatsOnSofas() view public returns(uint) {
+    uint sofaSum = 0;
+    for(uint i = 0; i < sofas.length; i++) {
+        sofaSum = SafeMath.add(sofaSum, sofas[i].seats);
+    }
+
+    return SafeMath.div(sofaSum, sofas.length);
+  }
+}
